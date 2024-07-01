9faf5a904a0689b600e0266bf16098bc80a6bcfb
Numan <numan.b@arise.tech>
2024-07-01 23:11:48 +0700

    case XIV should return 14

--- main_test.go
+++ main_test.go
@@ -13,6 +13,7 @@ func TestRomanToNumber(t *testing.T) {
 		{name: "case 2", args: "II", want: 2},
 		{name: "case 4", args: "IV", want: 4},
 		{name: "case 9", args: "IX", want: 9},
+		{name: "case 14", args: "XIV", want: 14},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
